e2823d48b84d933f85c1fe5ee4744ebc24199efa
rav3434 <ravish_cg@yahoo.com>
2022-10-23 10:24:23 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,47 @@
+const mysql=require('mysql');
+const express=require('express');
+const bodyparser = require('body-parser');
+const { DATABASE } = process.env;
+const { PASSWORD } = process.env;
+const { KEYHOST } = process.env;
+const { KEYUSER } = process.env;
+const { KEYPORT } = process.env;
+
+const app=express();
+
+var connection=mysql.createConnection({
+    host:KEYHOST,
+    user:KEYUSER,
+    password:PASSWORD,
+    database:DATABASE,
+    port:KEYPORT
+
+})
+connection.connect((err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log('connected')
+    }
+})
+
+// connection.query('select * from tabletest1', (err,results,fields)=>{
+//     if(err){
+//         return console.log(err)
+
+//     }
+//     return console.log(results)
+// })
+
+connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          console.log("server is not working")                    // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+
+
+module.exports=connection
---
+++ server.js
@@ -0,0 +1,149 @@
+var express=require("express")
+var app=express()
+const path=require('path')
+const http=require('http').createServer(app)
+var router = express.Router();
+
+require('dotenv').config()
+const connection = require('./db')
+
+var bodyParser=require("body-parser")
+//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+app.get('/seats', (req, res) => {
+
+   
+    connection.query("select * from `TABLE 8` ", function (err, data, fields) {
+        if (err) { throw (err) }
+        data = JSON.stringify(data)
+        
+       res.send(data)
+       
+})
+})
+
+function middleware(req,res,next){
+   
+    let count=0
+    let nulldata=[]
+    connection.query("select * from `TABLE 8`", function (err, data, fields) {
+        if (err) { throw (err) }
+
+    for(i=0;i<data.length;i++){
+        if(data[i].is_booked=='booked'){
+            count++
+        }
+        else{
+nulldata.push(data[i])
+        }
+    }
+
+    let percentage=(count/data.length*100)
+    //console.log(percentage)
+        req.data=percentage
+        req.emptyseats=nulldata
+        //console.log(req.emptyseats)
+        next()
+       
+})
+
+}
+
+
+app.get('/seats/:id', middleware,(req, res) => {
+
+   
+    connection.query("select * from `TABLE 9`", function (err, data, fields) {
+        if (err) { throw (err) }
+        console.log("the size is")
+        console.log(req.data.length)
+        
+var seatinfo={}
+
+
+
+
+        for(i=0;i<req.emptyseats.length;i++){
+if(req.emptyseats[i].id==req.params.id){
+ // console.log(req.emptyseats[i].id)
+    for(j=0;j<data.length;j++){
+        if(data[j].seat_class==req.emptyseats[i].seat_class){
+            seatinfo.seat_class=req.emptyseats[i].seat_class
+            seatinfo.is_booked=req.emptyseats[i].is_booked
+            seatinfo.seat_identifier=req.emptyseats[i].seat_identifier
+            price=data[j].min_price
+            
+            if(data[j].min_price!="" && req.data<=40){
+                seatinfo.price=data[j].min_price
+               break
+            }
+            if(data[j].normal_price!="" && req.data<=40){
+                seatinfo.price=data[j].normal_price
+              break
+            } 
+
+
+
+            if(data[j].normal_price!="" && req.data>40 && req.data<=60){
+                seatinfo.price=data[j].normal_price
+              break
+            } 
+            if(data[j].max_price!="" && req.data>40 && req.data<=60){
+                seatinfo.price=data[j].max_price
+              break
+            } 
+
+
+            if(data[j].max_price!="" && req.data>60){
+                seatinfo.price=data[j].max_price
+                break
+              
+            }  if(data[j].normal_price!="" && req.data>60){
+                seatinfo.price=data[j].normal_price
+              break
+            } 
+
+           else{
+            seatinfo.price="no ticket"
+           }
+        }
+    }
+
+}
+        }
+
+
+    
+        data = JSON.stringify(seatinfo)
+        
+
+        
+       res.send(data)
+       
+})
+})
+
+app.post("/booking",function(req,res){
+
+    console.log(req.body)
+
+    var mydata = "UPDATE `TABLE 8` SET username='" + req.body.username + "', phoneno='"+req.body.phoneno+"',is_booked='booked' WHERE id='"+req.body.id+"'";
+
+    connection.query(mydata, function (err, result) {
+        if (err) {throw err}
+        else{
+            res.send("ticket booked")
+        }
+
+        
+    })
+})
+
+
+
+const PORT=process.env.PORT
+
+http.listen(PORT||8800, () => {
+    console.log("listening")
+})
